349fdb38c36fa9bb37b45d2179482609d214a454
MicahBear <micah.b.elm@gmail.com>
2022-12-22 16:16:00 -0800

    create index.css: cset root styles./Add to header component: start of styling, jsx, and import to App component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <i className="fa-solid fa-globe header--icon"></i>
+      <h3 className="header--title">my travel notes</h3>
+    </div>
+  );
 }
