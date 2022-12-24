c4352d2d4150dd1e327f76fe206095d2ba7b2607
MicahBear <micah.b.elm@gmail.com>
2022-12-23 22:30:39 -0800

    test data

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: ""
+    }
+    ,
+    {
+        title: "Sydney Opera House",
+        location: "Austraila",
+        googleMapsUrl: "https://google.com",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: ""
+    },
+    {
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://google.com",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: ""
+    }
+]
