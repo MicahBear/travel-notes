80109a51dfdc9125b90de8e46c5077f16695963c
MicahBear <micah.b.elm@gmail.com>
2022-12-24 05:49:13 -0800

    update header and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <i className="fa-solid fa-globe header--icon"></i>
-      <h3 className="header--title">my travel notes</h3>
+      <h3 className="header--title">my travel notes.</h3>
     </div>
   );
 }
